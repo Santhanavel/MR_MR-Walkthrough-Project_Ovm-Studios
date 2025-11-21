ba10b6274dc177ea46bef8e4215be8bc033a22f5
SanthanavelR <132562317+SanthanavelR@users.noreply.github.com>
2025-11-21 21:44:43 +0530

    Anchor added

--- MR_MR Walkthrough/Assets/Scripts/MRPlaceAndAnchor.cs
+++ MR_MR Walkthrough/Assets/Scripts/MRPlaceAndAnchor.cs
@@ -1,21 +1,65 @@
-﻿using System;
+﻿using Meta.XR.BuildingBlocks;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MRPlaceAndAnchor : MonoBehaviour
 {
-    public OVRSpatialAnchor Anchor;
+    [Header("Prefab To Spawn")]
+    public GameObject spawnPrefab;
+
+    [Header("Anchor Building Blocks")]
+    public SpatialAnchorLoaderBuildingBlock anchorLoader;
+    public SpatialAnchorCoreBuildingBlock anchorCore;
+
+    [Header("Events")]
+    public UnityEvent OnMultipleAnchorsFound;
+
+    [Header("Debug")]
+    public List<OVRSpatialAnchor> anchors = new List<OVRSpatialAnchor>();
+
+    private bool placementEnabled = true;
 
     private void Awake()
     {
-        if(Anchor == null)
-        {
-            Anchor = FindObjectOfType<OVRSpatialAnchor>();
+        // Load anchors from local storage
+        anchorLoader.LoadAnchorsFromDefaultLocalStorage();
+        // Bind loader event
+        anchorCore.OnAnchorsLoadCompleted.AddListener(OnSavedAnchorsLoaded);
 
-            if(Anchor == null)
-            {
-                Anchor = new OVRSpatialAnchor();
-            }
+    }
+
+    private void OnSavedAnchorsLoaded(IReadOnlyList<OVRSpatialAnchor> loadedAnchors)
+    {
+        anchors.Clear();
+
+        foreach (var anchor in loadedAnchors)
+        {
+            anchors.Add(anchor);
+        }
+
+        Debug.Log("Loaded Anchors Count = " + anchors.Count);
+
+        if (anchors.Count > 1)
+        {
+            // MULTIPLE ANCHORS FOUND
+            Debug.Log("Multiple anchors found → clearing all.");
+
+            OnMultipleAnchorsFound?.Invoke();
+
+            // Delete anchors from world + storage
+            anchorCore.EraseAllAnchors();
+            anchors.Clear();
+            placementEnabled = true; // allow new placement
+        }
+        else if (anchors.Count == 1)
+        {
+            // ONLY ONE ANCHOR FOUND → Spawn object at anchor
+            Debug.Log("One anchor found → spawning object.");
+
+            Instantiate(spawnPrefab, anchors[0].transform.position,Quaternion.identity);
+            placementEnabled = false; // lock placement
         }
     }
 }
