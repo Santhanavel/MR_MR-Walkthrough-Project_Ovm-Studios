76c8317ed3735a7fc7ae8f0208cb1c06841daab1
SanthanavelR <132562317+SanthanavelR@users.noreply.github.com>
2025-11-21 14:07:58 +0530

    Assets added

---
+++ MR_MR Walkthrough/Assets/Editor/PrintModelBounds.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PrintModelBounds
+{
+    [MenuItem("Tools/Print Selected Model Bounds")]
+    static void PrintSelectedModelBounds()
+    {
+        GameObject go = Selection.activeGameObject;
+
+        if (go == null)
+        {
+            Debug.LogError("❌ No object selected! Please select your model root.");
+            return;
+        }
+
+        // Combine bounds of all renderers
+        var renderers = go.GetComponentsInChildren<Renderer>();
+
+        if (renderers.Length == 0)
+        {
+            Debug.LogError("❌ No Mesh Renderers found in selected object!");
+            return;
+        }
+
+        Bounds bounds = renderers[0].bounds;
+
+        foreach (Renderer r in renderers)
+            bounds.Encapsulate(r.bounds);
+
+        Vector3 size = bounds.size;
+
+        Debug.Log($"✅ Total Bounds Size for {go.name}:  X={size.x:F3}  Y={size.y:F3}  Z={size.z:F3}");
+    }
+}
---
+++ MR_MR Walkthrough/Assets/Scripts/MRPlaceAndAnchor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MRPlaceAndAnchor : MonoBehaviour
+{
+    public OVRSpatialAnchor Anchor;
+
+    private void Awake()
+    {
+        if(Anchor == null)
+        {
+            Anchor = FindObjectOfType<OVRSpatialAnchor>();
+
+            if(Anchor == null)
+            {
+                Anchor = new OVRSpatialAnchor();
+            }
+        }
+    }
+}
